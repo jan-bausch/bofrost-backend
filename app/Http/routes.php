2e51ff98412932d8ab9b01f1ddc8a0eb4728956e
Jan F. Bausch <jfmulti@gmail.com>
2017-06-10 13:56:58 +0200

    Added product api

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,24 @@
 $app->get('/', function () use ($app) {
     return $app->version();
 });
+
+$app->get('user/{id}', function ($id) {
+   $response = ["id"=>$id, "username" => "moritzbruder", "points" => 50];
+   return response()->json($response);
+});
+
+$app->get('product/{id}', function ($id) {
+   $path = dirname(dirname(dirname(__FILE__))) . "/storage/app/app_productlist_shop.xml";
+   $productXml = new SimpleXMLElement($path, null, true);
+
+   
+   foreach ($productXml->product as $product) {      
+      if ($product->ArtNumber == $id) {
+         $response = ["name" => (string) $product->Title, "description" => (string) $product->Description, "image" => (string) $product->ImgUrl];
+         return response()->json($response);
+      }
+   }
+});
+
+$app->get('product/{id}/completedChallenges', function ($id) {
+});
